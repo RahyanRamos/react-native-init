c4c87e4d18190b7dcfbae63857cc540e56d3b96f
Rahyan Ramos <rahyanramos.dev@gmail.com>
2025-03-28 21:57:51 -0400

    Add style in App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Incremento from "./Components/Incremento";
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <Incremento/>
